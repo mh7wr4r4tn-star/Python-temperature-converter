bd70bc7b6373e4a739559b2aa65aa1153e304b7c
Kam <mh7wr4r4tn@privaterelay.appleid.com>
2025-12-04 15:26:29 +0200

    Implement temperature conversion between Celsius and Fahrenheit

---
+++ Converter.py
@@ -0,0 +1,10 @@
+question=input("is your unit in Farenheit or Celsius? (C orF)": ")
+unit=float("what is the temperature?(enter the number): ")
+if unit="C":
+unit=({unit}*9)/5+32
+print("your new temp is {unit} degrees Farenheit")
+if unit="F":
+unit=({unit}-32)*5/9
+print("your new temp is {unit} degrees Celsius")
+  else:
+print("{unit} isnt valid"}
